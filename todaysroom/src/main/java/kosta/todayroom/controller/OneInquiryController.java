c09d6168dd03ce5ca22f967d7b6545414f6c31b1
mskim <mskim4348@naver.com>
2020-11-25 09:30:09 +0900

    msk push

--- todaysroom/src/main/java/kosta/todayroom/controller/OneInquiryController.java
+++ todaysroom/src/main/java/kosta/todayroom/controller/OneInquiryController.java
@@ -1,17 +1,22 @@
 package kosta.todayroom.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kosta.todayroom.domain.One_inquiryVO;
+import kosta.todayroom.service.OneInquiryService;
+import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
 @Controller
 @Log4j
 @RequestMapping("/oneInquriy")
 public class OneInquiryController {
+	@Setter(onMethod_=@Autowired)
+	private OneInquiryService service;
 	
 	@GetMapping("/register")
 	public void registerForm(){}
@@ -23,7 +28,9 @@ public class OneInquiryController {
 		
 		log.warn(vo);
 		
-		return "";
+		service.register(vo);
+		
+		return "redirect:/oneInquriy/register";
 	}
 	
 }
