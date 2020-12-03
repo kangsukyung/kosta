fd09e9c4d7b77e0193ff3e383b527c6db96d74ae
Hwang Hyeon Woo <wwoo1028@naver.com>
2020-12-03 11:04:57 +0900

    자동완성 기능 구현

--- todaysroom/src/main/java/kosta/todayroom/controller/SearchController.java
+++ todaysroom/src/main/java/kosta/todayroom/controller/SearchController.java
@@ -3,10 +3,13 @@ package kosta.todayroom.controller;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import kosta.todayroom.service.SearchService;
+import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
 @RestController
@@ -14,17 +17,14 @@ import lombok.extern.log4j.Log4j;
 @RequestMapping("/search/*")
 public class SearchController {
 	
+	@Setter(onMethod_=@Autowired)
+	SearchService service;
+	
 	@RequestMapping(value="/autocompletelist", produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public List<String> autocompletelist(String keyword){
-		System.out.println(keyword);
+		System.out.println("자동완성 키워드 : "+keyword);
 		
-		List<String> test = new LinkedList<>();
-		
-		test.add("test1");
-		test.add("test2");
-		test.add("test3");
-		
-		return test;
+		return service.autoCompleteList(keyword);
 	}
 	
 }
